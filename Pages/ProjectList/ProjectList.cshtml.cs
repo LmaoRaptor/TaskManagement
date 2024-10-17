eee8fd38c8f391bf5699b71f5ae91ed910308b89
Tien Le Manh <tienlm12@fpt.com>
2024-10-17 23:36:31 +0700

    first commit

---
+++ Pages/ProjectList/ProjectList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221_Project.Pages.ProjectList
+{
+    public class ProjectListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/TaskList/TaskList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221_Project.Pages.TaskList
+{
+    public class TaskListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
